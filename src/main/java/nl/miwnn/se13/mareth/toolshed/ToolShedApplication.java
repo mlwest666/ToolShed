db9573280403659bea5e793d66f7aa9fae3333f3
mlwest666 <mareth_w@hotmail.com>
2024-03-28 11:36:51 +0100

    Initial commit

---
+++ src/main/java/nl/miwnn/se13/mareth/toolshed/ToolShedApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se13.mareth.toolshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolShedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolShedApplication.class, args);
+	}
+
+}
